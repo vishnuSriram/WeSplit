d55e431fd9ea303cab33e95e3c35cc7764a866f2
Vishnu Sriram <vsriram42@gmail.com>
2023-07-25 11:34:30 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vishnu Sriram on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
